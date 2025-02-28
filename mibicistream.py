4fd5679525bdfeffee7d48a53c6285e6b5f38a9c
SDES-050303 <sergio.elizondo@iteso.mx>
2025-02-27 22:40:11 -0600

    Update mibicistream.py

--- mibicistream.py
+++ mibicistream.py
@@ -5,8 +5,7 @@ import seaborn as sns
 import numpy as np
 from geopy.distance import geodesic
 
-st.sidebar.image("./IMG/Foto de estacion mi bici.jpg", use_container_width=True)
-
+st.image("./IMG/Foto de estacion mi bici.jpg", use_container_width=True)
 
 st.title("Análisis Mibici")
 st.header("Introducción")
